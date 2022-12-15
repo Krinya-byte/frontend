10dd70ef75c8ea8b24f7be364160faabaf7afbad
Kristóf Török <torokkristof692@gmail.com>
2022-12-15 14:59:41 +0100

    httpservice and proxy

---
+++ src/app/common/http.service.ts
@@ -0,0 +1,36 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {Observable, Observer} from "rxjs";
+import {environment} from "../../environment/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  private readonly url: string = "";
+  private httpHeader: HttpHeaders = new HttpHeaders().set("key", "value");
+  private httpParam: HttpParams = new HttpParams();
+
+  constructor(public http: HttpClient) {
+    this.url = environment.basicapiPoint;
+  }
+
+  get<T>(route: string, params?: HttpParams): Observable<T | Object> {
+
+    return this.http.get(`${this.url + route}`, {headers: this.httpHeader, params: params ? params : this.httpParam});
+
+  }
+
+  post<T>(route: string, body: any): Observable<T | Object> {
+
+    return this.http.post(`${this.url + route}`, body, {headers: this.httpHeader});
+
+  }
+
+  delete<T>(route: string): Observable<T | Object> {
+
+    return this.http.post(`${this.url + route}`, {headers: this.httpHeader});
+
+  }
+
+}
